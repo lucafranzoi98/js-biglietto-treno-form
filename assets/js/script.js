0cbc7df674e502b11fe416611ec815e196c7f6b3
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-08-24 17:07:48 +0200

    add some style, form and bonus part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const calcElement = document.getElementById("calc");
-calcElement.addEventListener("click", function(){
+calcElement.addEventListener("click", function(e){
    
    const userDistance = Number(document.getElementById("userDistance").value);
    const userAge = document.getElementById("selectAge").value;
+
+   console.log(userAge);
 
    let price = userDistance * 0.21;
 
@@ -17,5 +19,6 @@ calcElement.addEventListener("click", function(){
    price = price.toFixed(2);
 
    document.getElementById("userPrice").innerHTML = price;
+   e.preventDefault();
 }
 );
